b8216fdf84ef9e6f93aecf012b8deeca909ca2a1
mwang20 <mwang20@stuy.edu>
2021-01-14 00:23:54 -0500

    Created my radixSortSimple method. Created helper method getMaximum.

--- Radix.java
+++ Radix.java
@@ -16,15 +16,42 @@ public class Radix{
   
   public static int length(int n){
     int col = 0;
-    while (Math.pow(10, col) <= n){
+    while (Math.pow(10, col) <= Math.abs(n)){
       col++;
     }
     return col;
   }
   
-  public static void merge(MyLinkedList original, MyLinkedList[]buckets){
+  public static void merge(SortableLinkedList original, SortableLinkedList[]buckets){
     for (int i = 0; i < buckets.length; i++){
-      original.add(buckets[i]);
+      original.extend(buckets[i]);
     }
+  }
+  
+  public static int getMaximum(SortableLinkedList data){
+    int max = data.get(0);
+    for (int i = 0; i < data.size(); i++){
+      if (data.get(i) > max){
+        max = data.get(i);
+      }
+    }
+    return max;
+  }
+  public static void radixSortSimple(SortableLinkedList data){
+    SortableLinkedList[] buckets = new SortableLinkedList[10];
+    int max = getMaximum(data);
+    
+    for (int i = 0; i < length(max); i++){
+      
+      for (int j = 0; j < data.size(); j++){
+        
+        int current = data.get(j);
+        int nth = nth(current, i);
+        buckets[nth].add(current);
+        
+      }
+    }
+    data = new SortableLinkedList();
+    merge(data, buckets);
   }
 }
